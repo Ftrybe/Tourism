db35308c4446def8ac96f04c841657ac4c0852ed
ftrybe <fcbyeah@yeah.net>
2019-01-05 21:25:32 +0800

    添加收藏点赞组件

--- src/app/share.module.ts
+++ src/app/share.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {BannerComponent} from './common/banner/banner.component';
+import {ToolbarComponent} from './common/toolbar/toolbar.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
   declarations: [
-    BannerComponent
+    BannerComponent,
+    ToolbarComponent
   ],
   exports: [
-    BannerComponent
+    BannerComponent,
+    ToolbarComponent
   ]
 })
 export class ShareModule { }
